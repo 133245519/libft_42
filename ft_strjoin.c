5d94a07a80d314a60d1bdff239dff33f5644bfc4
Mohammed El Harchi <mel-harc@e2r3p15.1337.ma>
2023-02-09 05:49:49 +0100

    v

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,30 +12,30 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(const char *s1, const char *s2)
-{
-	char	*str;
-	size_t	lens1;
-	size_t	lens2;
-	size_t	i;
-	size_t	j;
+// char	*ft_strjoin(const char *s1, const char *s2)
+// {
+// 	char	*str;
+// 	size_t	lens1;
+// 	size_t	lens2;
+// 	size_t	i;
+// 	size_t	j;
 
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
-		return (0);
-	lens1 = ft_strlen(s1);
-	lens2 = ft_strlen(s2);
-	str = (char *)malloc((lens1 + lens2) * sizeof(char) + 1);
-	if (!str)
-		return (NULL);
-	while (i < lens1)
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (j < lens2)
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	return (str);
-}
+// 	i = 0;
+// 	j = 0;
+// 	if (!s1 || !s2)
+// 		return (0);
+// 	lens1 = ft_strlen(s1);
+// 	lens2 = ft_strlen(s2);
+// 	str = (char *)malloc((lens1 + lens2) * sizeof(char) + 1);
+// 	if (!str)
+// 		return (NULL);
+// 	while (i < lens1)
+// 	{
+// 		str[i] = s1[i];
+// 		i++;
+// 	}
+// 	while (j < lens2)
+// 		str[i++] = s2[j++];
+// 	str[i] = '\0';
+// 	return (str);
+// }
